7db845f6f96ddbdab97695b9e4b7488cc3385be5
Ilya <Ilya773@vk.com>
2022-05-13 09:21:20 +0400

    init

---
+++ src/test/java/lipatov/lab/LabApplicationTests.java
@@ -0,0 +1,13 @@
+package lipatov.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
